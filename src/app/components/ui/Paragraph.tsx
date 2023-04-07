6c6219151a2e4afd9f0fa19097448d3a10e27fd8
Kamalesh-Dev-Web <kamalsurea619@gmail.com>
2023-04-07 16:52:59 +0530

    Intial setup is done

---
+++ src/app/components/ui/Paragraph.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { cva } from "class-variance-authority";
+
+interface ParagraphProps {}
+
+const Paragraph: FC<ParagraphProps> = () => {
+  return <>Paragraph</>;
+};
+
+export default Paragraph;
